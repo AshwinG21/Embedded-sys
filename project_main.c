5e4418455d68791f90ee180633df7ec056cffa8f
Uttam R <82046396+259881@users.noreply.github.com>
2021-04-29 20:19:34 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,53 +1,66 @@
 /**
- * @file project_main.c
- * @author Bharath.G ()
- * @brief Project to Blink an LED at 1000ms ON and 500 ms OFF Interval
+ * @file HeatControlSystem_Main.c
+ * @author Hemanth A (hemanth.ec17@bmsce.ac.in)
+ * @brief The main program to execute the heat control system of a seat inside a car
  * @version 0.1
- * @date 2021-04-23
+ * @date 2021-04-27
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-#include "project_config.h"
-
-#include "user_utils.h"
-#include "blinky.h"
-
-/**
- * @brief Initialize all the Peripherals and pin configurations
- * 
- */
-void peripheral_init(void)
-{
-	/* Configure LED Pin */
-	DDRB |= (1 << DDB0);
-}
-
-void change_led_state(uint8_t state)
-{
-	LED_PORT = (state << LED_PIN);
-}
-
+#include <avr/io.h>
+#include <util/delay.h>
+#include "ButtonSensorHeaterLED.h"
+#include "TemperatureSensorADC.h"
+#include "ADC_PWMoutput.h"
+#include "SerialCommunicationUART.h"
 
 /**
- * @brief Main function where the code execution starts
+ * @brief Main function
  * 
- * @return int Return 0 if the program completes successfully
- * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
- * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
+ * @return int 
  */
 int main(void)
 {
-	/* Initialize Peripherals */
-	peripheral_init();
+    Buttons_LED_Init(); //Initialise the button input registers
+    Init_ADC(); //Initialise the ADC
+    TimerWaveGenMode(); //Initialise the registers for TIMER1 as fast PWM
+    UARTinit(103); //Initialise UART registers
+    uint16_t temp;
 
-	for(;;)
-	{
-        change_led_state(LED_ON);
-		delay_ms(LED_ON_TIME);
-		
-        change_led_state(LED_OFF);
-		delay_ms(LED_OFF_TIME);	
-	}
-	return 0;
+    /**
+     * @brief Infinite loop to run the microcontroller
+     * 
+     */
+    while(1)
+    {
+       if(BUTTON_SENSOR_ON) //Checking if the input bit to 7th bit of pinB is made 0 from 1 by pressing led
+        {
+
+            if(HEATER_ON) //Checking if the input bit to 6th bit of pinB is made 0 from 1 by pressing led
+            {
+                _delay_ms(20);
+                SET_LED; //make 0th bit of port B as 1, makes led glow
+                temp = Read_ADC(0);
+                outputbyPWM(temp);
+                _delay_ms(20);
+                
+            }
+            else
+            {
+                _delay_ms(20);
+                OCR1A = 0; //make PWM output 0 if switch is off
+                CLEAR_LED; // make led off
+            }
+        }
+        else
+        {
+                
+                CLEAR_LED; //make led off
+                OCR1A = 0; //make PWM output 0 if both switches are off
+                _delay_ms(20);
+        }
+    }
+
+ return 0;   
 }
