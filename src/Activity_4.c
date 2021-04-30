706bd3c50731c6254548d571b1f17c96d2274044
Uttam R <82046396+259881@users.noreply.github.com>
2021-04-30 12:56:41 +0530

    Update Activity_4.c

--- src/Activity_4.c
+++ src/Activity_4.c
@@ -1,49 +1,46 @@
-/**
- * @brief A file where serial communication functions are implemented
- * 
- */
 #include <avr/io.h>
+#include "Activity_2.h"
+#include "Activity_3.h"
 #include "Activity_4.h"
-/**
- * @brief A program to initialize the UART serial communication
- * 
- * @param ubrr_value 
- */
-void UARTinit(uint16_t ubrr_value){
 
+void USARTInit(uint16_t ubrr_value)
+{
     UBRR0L = ubrr_value;
     UBRR0H = (ubrr_value>>8)&(0x00ff);
-    UART_CHARACTER_SIZE; // 8 bit size of data
-    UART_ENABLED; //enable rx and tx of uart with interrupts
+    UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ01);
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 
 }
-
-/**
- * @brief A function to read characters coming from other UART port
- * 
- * @return char 
- */
-char UARTreadchar(){
-
-while(UART_DATA_NOT_RECEIVED){
-
+void USARTWrite(char temp_value)
+{
+  while(!(UCSR0A & (1<<UDRE0)))
+  { }
+UDR0 = temp_value;
 }
 
-return UDR0;
+char temp_disp()
+{
+    char temperature;
 
-}
+     if((ReadADC(0)>=0) && (ReadADC(0)<=209)){
 
-/**
- * @brief A function to write characters to send it to other UART port
- * 
- * @param data 
- */
-void UARTwritecharacter(char data){
+       temperature= 20;
+    }
+    else if((ReadADC(0)>=210) && (ReadADC(0)<=509)){
 
-while(UART_DATA_NOT_WRITTEN){
+        temperature= 25;
+    }
+    else if((ReadADC(0)>=510) && (ReadADC(0)<=709)){
 
-}
+        temperature= 29;
+    }
+    else if((ReadADC(0)>=710) && (ReadADC(0)<=1024)){
 
-UDR0 = data;
-
+        temperature= 33;
+    }
+    else
+    {
+        temperature= 0;
+    }
+    return temperature;
 }
