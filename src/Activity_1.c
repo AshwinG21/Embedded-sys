8e74acff8b7d017160c522d78a43c50731abddc5
Uttam R <82046396+259881@users.noreply.github.com>
2021-04-30 12:54:09 +0530

    Update Activity_1.c

--- src/Activity_1.c
+++ src/Activity_1.c
@@ -1,22 +1,10 @@
-/**
- * @brief A file where the functions for button and heater are implememted
- *
- */
-
 #include <avr/io.h>
-#include <util/delay.h>
 #include "Activity_1.h"
 
-/**
- * @brief A function to operate the button and heater sensor
- * 
- */
-void Buttons_LED_Init()
+void initialise()//TO INITIALISE PORTS
 {
-SET_LED_AS_OUTPUT; //set led as output port
-SET_BUTTON_AS_INPUT; //clear bit 6 of DDR B
-PULLUP_BUTTON; //Set bit 6 of Port B
-SET_HEATER_AS_INPUT; //clear bit 7 of DDR B
-PULLUP_HEATER;//Set bit 7 of DDR B
-
+    DDRB|=(1<<PB0);//TO MAKE B0 AS AN OUTPUT PIN
+    DDRD&=~(3<<PD0);//TO MAKE Do and D1  AS AN INPUT PIN
+    PORTD|=(3<<PD0);//PULL UP
+  
 }
