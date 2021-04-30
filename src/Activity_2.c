c95c9e444e24d5abf93200dfd8e5fdfd8041196b
Uttam R <82046396+259881@users.noreply.github.com>
2021-04-30 12:54:59 +0530

    Update Activity_2.c

--- src/Activity_2.c
+++ src/Activity_2.c
@@ -1,36 +1,23 @@
-/**
- * @brief A file where the ADC functions are implemented for temperature sensor
- * 
- */
 #include <avr/io.h>
 #include "Activity_2.h"
 
-/**
- * @brief A function to initialise the analog to digital converter
- * 
- */
-void Init_ADC(){
-    SET_REFERENCE_VOLTAGE;
-    ADC_ENABLE_PRESCALE;
 
-}
-
-/**
- * @brief A funciton to receive temperature sensor inputs from analog pin and output it to a register ADC which has 2 8 bit registers ADCL and ADCH.
- * 
- * @param ch 
- * @return uint16_t 
- */
-uint16_t Read_ADC(uint8_t ch)
+void InitADC()
 {
-    ADMUX&=0xf8;
-    ch = ch&0b00000111;
+    ADMUX=(1<<REFS0); //ADref=AVcc
+    ADCSRA=(1<<ADEN)|(7<<ADPS0);
+}
+uint16_t ReadADC(uint8_t ch)
+{
+
+    ADMUX&=0xf8;//SELECT ADC CHANNEL CH MUST BE 0-7
+    ch=ch&0b00000111;
     ADMUX|=ch;
-
+    //start single conversion
     ADCSRA|=(1<<ADSC);
-
-    while(ADC_INTERRUPT_NOT_OCCURED);
-    SET_ADC_INTERRUPT_FLAG;
+    //WAIT FOR CONVERSION TO COMPLETE
+    while(!(ADCSRA&(1<<ADIF)));
+    //CLEAR ADIF BY WRITING ONE TO IT
+    ADCSRA|=(1<<ADIF);
     return(ADC);
-
 }
